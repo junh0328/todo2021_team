acbd867a244782b69097b46377842ef7ca6e1f52
이준희 <junh0328@naver.com>
2020-11-14 15:17:37 +0900

    todolist 복기및 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Todoinsert from "./components/Todoinsert";
+import TodoList from "./components/TodoList";
+import Todotemplate from "./components/Todotemplate";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Todotemplate>
+        <Todoinsert />
+        <TodoList />
+      </Todotemplate>
     </div>
   );
 }
